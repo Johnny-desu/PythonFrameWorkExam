515ccc0159871135155904579e2a010a47d56b11
Johnny-desu <user@wxample.com>
2023-11-30 15:25:06 +0900

    shine

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sbdi*(%n0*7uj%-^5hfz*l(w11z0u=zgs(z4@4v_z-78&##x*='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
