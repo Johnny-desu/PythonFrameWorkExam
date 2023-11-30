fe1e6b3002b9129da1c1f699988df89e42956f44
Johnny-desu <user@wxample.com>
2023-11-30 15:30:43 +0900

    fuck you

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATIC_ROOT = '/home/ubuntu/static/'
+STATIC_ROOT = '/home/ubuntu/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
